c29761fe34aa0aba3dcf101ffd790d70bc7d4c7d
woramun wattana <hernfhaja@gmail.com>
2022-06-01 04:07:26 +0700

    fullfunction no css

--- src/App.vue
+++ src/App.vue
@@ -1,90 +1,27 @@
 <template>
   <div id="app">
     <UploadImage />
-    <div
-      id="my-strictly-unique-vue-upload-multiple-image"
-      style="display: flex; justify-content: center"
-    >
-      <VueUploadMultipleImage
-        @upload-success="uploadImageSuccess"
-        @before-remove="beforeRemove"
-        @edit-image="editImage"
-        @mark-is-primary="markIsPrimary"
-        @limit-exceeded="limitExceeded"
-        id-upload="myIdUpload"
-        id-edit="myIdEdit"
-        :data-images="images"
-        :max-image="20"
-        primary-text=""
-        browse-text="คลิกตรงนี้เพื่อส่งรูป"
-        mark-is-primary-text="ให้เป็นรูปแรก"
-        popup-text="รูป"
-        dragText="กรุณาเลือกรูปของท่าน"
-        :multiple="true"
-        :show-edit="true"
-        :show-delete="true"
-        :show-add="true"
-      />
-    </div>
-
-    <button @click="onUpload">upload</button>
   </div>
 </template>
 
 <script>
-import UploadImage from "./components/UploadImage.vue";
-import VueUploadMultipleImage from "vue-upload-multiple-image";
-// import {
-//   getStorage,
-//   ref,
-//   uploadBytesResumable,
-//   getDownloadURL,
-// } from "firebase/storage";
+import UploadImage from "./components/UploadImage";
 
 export default {
   name: "App",
   components: {
     UploadImage,
-    VueUploadMultipleImage,
   },
   data() {
-    return {
-      images: [{ image: 1, id: 1 }],
-      test: { name: "hahah", id: 12 },
-    };
+    return {};
   },
-  methods: {
-    uploadImageSuccess(formData, index, fileList) {
-      console.log("formData", formData);
-      console.log("index", index);
-      console.log("fileList", fileList[0]);
-    },
-    beforeRemove(index, removeCallBack) {
-      console.log("index", index);
-      var r = confirm("remove image");
-      if (r == true) {
-        removeCallBack();
-      }
-    },
-    editImage(formData, index, fileList) {
-      console.log("edit data", formData, index, fileList);
-    },
-    markIsPrimary(index, fileList) {
-      console.log("markIsPrimary data", index, fileList);
-    },
-    limitExceeded(amount) {
-      console.log("limitExceeded data", amount);
-    },
-    onUpload(e) {
-      console.log(e);
-    },
-  },
+  methods: {},
 };
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
--- src/components/UploadImage.vue
+++ src/components/UploadImage.vue
@@ -1,17 +1,197 @@
 <template>
   <div class="uploadImage">
-    <h1>Upload multiple image</h1>
+    <!-- popup -->
+    <div id="popup" class="popup" :style="{ display: popupData.display }">
+      <h3>ส่งสำเร็จ</h3>
+      <button @click="reloadPage">กด ส่งอีกรูป</button>
+    </div>
+
+    <!-- uploadimage -->
+    <div class="text-3xl font-bold underline">
+      <h1>ส่งรูป ผู้ร่วงลับ</h1>
+      <div class="div-uploadImage">
+        เลือกรูปภาพ
+        <input
+          id="uploadImage"
+          class="input-uploadImage"
+          type="file"
+          @change="previewImage"
+          accept="image/*"
+        />
+      </div>
+    </div>
+    <img id="uploadPreview" class="uploadPreview" />
+    <div v-if="imageData != null">
+      <p>
+        ดำเนินการ: {{ uploadValue.toFixed() + "%" }}
+        <progress id="progress" :value="uploadValue" max="100"></progress>
+      </p>
+      <br />
+      <button @click="onUpload" class="button-sendImage">ส่งภาพ</button>
+    </div>
   </div>
 </template>
-
+<script src="https://cdn.tailwindcss.com"></script>
 <script>
+import {
+  getStorage,
+  ref,
+  uploadBytesResumable,
+  getDownloadURL,
+} from "firebase/storage";
+
 export default {
-  name: "UploadImage",
-  props: {
-    msg: String,
+  name: "uploadImage",
+  data() {
+    return {
+      imageData: null,
+      picture: null,
+      uploadValue: 0,
+      storage: getStorage(),
+      storageRef: null,
+      popupData: {
+        display: "none",
+      },
+    };
+  },
+
+  methods: {
+    previewImage(event) {
+      const oFReader = new FileReader();
+      oFReader.readAsDataURL(document.getElementById("uploadImage").files[0]);
+
+      oFReader.onload = function (oFREvent) {
+        document.getElementById("uploadPreview").src = oFREvent.target.result;
+      };
+
+      this.uploadValue = 0;
+      this.picture = event.file;
+      this.imageData = event.target.files[0];
+    },
+
+    onUpload() {
+      const file = this.imageData;
+
+      const storage = this.storage;
+      const storageRef = ref(storage, `${file.name}`);
+      const uploadTask = uploadBytesResumable(storageRef, file, metadata);
+
+      const metadata = {
+        contentType: "image/jpeg",
+      };
+
+      uploadTask.on(
+        "state_changed",
+        (snapshot) => {
+          // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+          const progress =
+            (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+          console.log("Upload is " + progress + "% done");
+          this.uploadValue = progress;
+          switch (snapshot.state) {
+            case "paused":
+              console.log("Upload is paused");
+              break;
+            case "running":
+              console.log("Upload is running");
+              break;
+          }
+        },
+        (error) => {
+          // A full list of error codes is available at
+          // https://firebase.google.com/docs/storage/web/handle-errors
+          switch (error.code) {
+            case "storage/unauthorized":
+              // User doesn't have permission to access the object
+              console.log("storage/unauthorized", error.code);
+              break;
+            case "storage/canceled":
+              // User canceled the upload
+              console.log("storage/canceled", error.code);
+              break;
+
+            // ...
+
+            case "storage/unknown":
+              // Unknown error occurred, inspect error.serverResponse
+              console.log("storage/unknown", error.code);
+              break;
+          }
+        },
+        () => {
+          // Upload completed successfully, now we can get the download URL
+          getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+            console.log("File available at", downloadURL);
+            this.picture = downloadURL;
+          });
+        }
+      );
+
+      this.openpopup();
+    },
+    openpopup() {
+      this.popupData.display = "block";
+    },
+    closeFunction() {
+      this.popupData.display = "none";
+    },
+    reloadPage() {
+      window.location.reload();
+    },
   },
 };
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped></style>
+<style>
+img.preview {
+  width: 200px;
+}
+
+.div-uploadImage {
+  padding: 5px 10px;
+  background: #00ad2d;
+  border: 1px solid #00ad2d;
+  position: relative;
+  color: #fff;
+  border-radius: 2px;
+  text-align: center;
+  float: center;
+  cursor: pointer;
+}
+
+.input-uploadImage {
+  position: absolute;
+  z-index: 1000;
+  opacity: 0;
+  cursor: pointer;
+  right: 0;
+  top: 0;
+  height: 100%;
+  font-size: 24px;
+  width: 100%;
+}
+
+.uploadPreview {
+  padding: 10px;
+  width: 500px;
+}
+
+.button-sendImage {
+  background: #00ad2d;
+}
+
+#popup {
+  padding: 20px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  position: absolute;
+  width: 30%;
+  height: 10%;
+  top: 35%;
+  border: black;
+  left: 30%;
+  background-color: rgba(241, 215, 173);
+  border-radius: 5px;
+}
+</style>
