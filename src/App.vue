51a4a32533d1b47f26e5092c1a7276c1de255f7b
woramun wattana <hernfhaja@gmail.com>
2022-05-31 20:45:28 +0700

    add firebase

--- src/App.vue
+++ src/App.vue
@@ -1,58 +1,45 @@
 <template>
   <div id="app">
     <UploadImage />
-    <VueUploadMultipleImage
-      @upload-success="uploadImageSuccess"
-      @before-remove="beforeRemove"
-      @edit-image="editImage"
-      @mark-is-primary="markIsPrimary"
-      @limit-exceeded="limitExceeded"
-      :data-images="images"
-      id-upload="myIdUpload"
-      id-edit="myIdEdit"
-      :max-image="20"
-      primary-text="Default"
-      browse-text="Drag pictures"
-      mark-is-primary-text="Set as default"
-      popup-text="รูป"
-      :multiple="true"
-      :show-edit="true"
-      :show-delete="true"
-      :show-add="true"
-    />
+    <div
+      id="my-strictly-unique-vue-upload-multiple-image"
+      style="display: flex; justify-content: center"
+    >
+      <VueUploadMultipleImage
+        @upload-success="uploadImageSuccess"
+        @before-remove="beforeRemove"
+        @edit-image="editImage"
+        @mark-is-primary="markIsPrimary"
+        @limit-exceeded="limitExceeded"
+        id-upload="myIdUpload"
+        id-edit="myIdEdit"
+        :data-images="images"
+        :max-image="20"
+        primary-text=""
+        browse-text="คลิกตรงนี้เพื่อส่งรูป"
+        mark-is-primary-text="ให้เป็นรูปแรก"
+        popup-text="รูป"
+        dragText="กรุณาเลือกรูปของท่าน"
+        :multiple="true"
+        :show-edit="true"
+        :show-delete="true"
+        :show-add="true"
+      />
+    </div>
+
+    <button @click="onUpload">upload</button>
   </div>
 </template>
-<script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
+
 <script>
 import UploadImage from "./components/UploadImage.vue";
 import VueUploadMultipleImage from "vue-upload-multiple-image";
-// import googleapi from "googleapi";
-
-// const { google } = require("googleapis");
-// const path = require("path");
-// const fs = require("fs");
-
-// const CLIENT_ID =
-//   "1079548728317-tg1d2na7pfqnqvorigjcd9koe95ek5qa.apps.googleusercontent.com";
-// const CLIENT_SECRET = "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";
-// const REDIRECT_URI = "https://developers.google.com/oauthplayground";
-// const REFRESH_TOKEN =
-//   "1//04xgw4yDGxWYkCgYIARAAGAQSNwF-L9IrNjxcd54k4I0bV_o7nOIRXyqv6hyFCXy3U_ttXx2ZNHI1x52tshuKxYSGA8S3ztit7vA";
-
-// const oauth2Client = new google.auth.OAuth2(
-//   CLIENT_SECRET,
-//   REDIRECT_URI,
-//   REFRESH_TOKEN
-// );
-
-// oauth2Client.setCredentials({ refresh_token: REFRESH_TOKEN });
-
-// const drive = google.drive({
-//   version: "v3",
-//   auth: oauth2Client,
-// });
-
-// const filePath = path.join(__dirname, "./assets/logo.png");
+// import {
+//   getStorage,
+//   ref,
+//   uploadBytesResumable,
+//   getDownloadURL,
+// } from "firebase/storage";
 
 export default {
   name: "App",
@@ -60,37 +47,17 @@ export default {
     UploadImage,
     VueUploadMultipleImage,
   },
+  data() {
+    return {
+      images: [{ image: 1, id: 1 }],
+      test: { name: "hahah", id: 12 },
+    };
+  },
   methods: {
     uploadImageSuccess(formData, index, fileList) {
-      console.log("upload success data ", formData, index, fileList);
-      // UploadImage api
-      // axios
-      //   .post("", formData)
-      //   .then((res) => {
-      //     console.log(res);
-      //   })
-      //   .catch(function (error) {
-      //     console.log(error);
-      //   });
-
-      //google drive
-      // async function uploadFile() {
-      //   try {
-      //     const res = await drive.files.crete({
-      //       requestBody: {
-      //         name: "logo.png",
-      //         mimeType: "image/png",
-      //       },
-      //       media: {
-      //         mimeType: "image/png",
-      //         body: fs.createReadStream(filePath),
-      //       },
-      //     });
-      //     console.log(response.data);
-      //   } catch (error) {
-      //     console.log(error);
-      //   }
-      // }
+      console.log("formData", formData);
+      console.log("index", index);
+      console.log("fileList", fileList[0]);
     },
     beforeRemove(index, removeCallBack) {
       console.log("index", index);
@@ -107,6 +74,9 @@ export default {
     },
     limitExceeded(amount) {
       console.log("limitExceeded data", amount);
+    },
+    onUpload(e) {
+      console.log(e);
     },
   },
 };
--- src/main.js
+++ src/main.js
@@ -1,12 +1,37 @@
 import Vue from "vue";
 import App from "./App.vue";
 import VueLazyload from "vue-lazyload";
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getAuth } from "firebase/auth";
+import { getStorage } from "firebase/storage";
 
 Vue.use(VueLazyload);
 Vue.config.productionTip = false;
 
+const firebaseConfig = {
+  apiKey: "AIzaSyBuyakkpFZPQg-dOiEJ796w6MpZAlgNPo0",
+  authDomain: "utidboon-d6b4c.firebaseapp.com",
+  projectId: "utidboon-d6b4c",
+  storageBucket: "utidboon-d6b4c.appspot.com",
+  messagingSenderId: "132558938003",
+  appId: "1:132558938003:web:fe88fea482e14774c7d266",
+  measurementId: "G-1RK8SBKLKM",
+};
+
+// Initialize Firebase
+/* eslint-disable */
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const auth = getAuth(app);
+const storage = getStorage(app);
+
 export default {
   components: {},
+  app,
+  analytics,
+  auth,
+  storage,
 };
 new Vue({
   render: (h) => h(App),
